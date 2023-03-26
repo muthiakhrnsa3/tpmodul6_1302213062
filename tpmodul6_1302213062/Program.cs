c5f7d30c198e5de5afb771bf80b48896329d94b5
muthiakhrnsa3 <127019755+muthiakhrnsa3@users.noreply.github.com>
2023-03-26 22:20:26 +0700

    Add project files.

---
+++ tpmodul6_1302213062/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace tpmodul6_1302213062;
+using System;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - [Muthia Khairunnisa_PRAKTIKAN]");
+
+        //test prekondisi
+        //video = new SayaTubeVideo(null);
+        //video = new SayaTubeVideo("Judul video dengan panjang lebih dari 100 karakter akan menghasilkan ContractException");
+
+        for (int i = 0; i < 1000000000; i += 10000000)
+        {
+            video.IncreasePlayCount(10000000);
+        }
+        video.PrintVideoDetails();
+    }
+}
